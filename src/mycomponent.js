1d1e781e39331fc1ff20049d410b73450b405ff6
kesleryak <kesleryak@gmail.com>
2024-07-17 20:05:46 +0800

    Squeezing Inputs

--- src/mycomponent.js
+++ src/mycomponent.js
@@ -7,12 +7,17 @@ function MyComponent() {
         <h1>Pythagoras Calculator</h1>
         <img src="https://i0.wp.com/pythagoras.nu/wp-content/uploads/2019/03/pythagoras-theorem.png?fit=480%2C352&ssl=1"/>
         <p>This is a Pythagoras Calculator. Place in 3 coordinates and find out if those coordinates make a right-angle triangle.</p>
-        <input type='number' id='x1' placeholder='x1'></input>
-        <input type='number' id='y1' placeholder='y1'></input>
-        <input type='number' id='x2' placeholder='x2'></input>
-        <input type='number' id='y2' placeholder='y2'></input>
-        <input type='number' id='x3' placeholder='x3'></input>
-        <input type='number' id='y3' placeholder='y3'></input>
+        <table>
+            <tr>
+                <td><input type='number' id='x1' placeholder='x1'></input></td><td><input type='number' id='y1' placeholder='y1'></input></td>
+            </tr>
+            <tr>
+                <td><input type='number' id='x2' placeholder='x2'></input></td><td><input type='number' id='y2' placeholder='y2'></input></td>
+            </tr>
+            <tr>
+                <td><input type='number' id='x3' placeholder='x3'></input></td><td><input type='number' id='y3' placeholder='y3'></input></td>
+            </tr>
+        </table>
         <button type='button' id='Calculate' onClick={() => {
             var x1 = document.getElementById('x1').value;
             var x2 = document.getElementById('x2').value;
@@ -34,9 +39,13 @@ function MyComponent() {
 
         }}>Calculate</button>
         <p>You can also type in the lengths of the sides of the triangle below.</p>
-        <input type='number' id='length1' placeholder='length1'></input>
-        <input type='number' id='length2' placeholder='length2'></input>
-        <input type='number' id='length3' placeholder='length3'></input>
+        <table>
+            <tr>
+                <td><input type='number' id='length1' placeholder='length1'></input></td>
+                <td><input type='number' id='length2' placeholder='length2'></input></td>
+                <td><input type='number' id='length3' placeholder='length3'></input></td>
+            </tr>
+        </table>
         <button type='button' id='Calculate' onClick={() => {
             let pyt = [0, 0, 0];
             pyt[0] = (document.getElementById('length1').value**2);
